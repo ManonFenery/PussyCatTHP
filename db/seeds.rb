c1afce4ee271cadb6441fbcc132a3c43bd4411b0
manonfenery <fenery.manon@gmail.com>
2019-03-05 13:55:47 +0100

    better routes, links and no more errors for cart

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,15 +7,21 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
 
+User.destroy_all
 Item.destroy_all
+Cart.destroy_all
+Order.destroy_all
+LineItem.destroy_all
 
-20.times do 
+20.times do
+	user = User.create(email: 'john@gmail.com', password: 'topsecret', password_confirmation: 'topsecret')
 	item = Item.create(title: Faker::Creature::Cat.name, description: Faker::Creature::Cat.registry, price: Faker::Number.decimal(2), img_url: Faker::Internet.url('example.com'))
 end
 
 
 
-5.times do
-	cart = Cart.create
-	line_item = LineItem.create(quantity: 2, item_id: Item.all.sample.id, cart_id: Cart.all.sample.id)
+10.times do
+	cart = Cart.create(user_id: User.all.sample.id)
+	order = Order.create(name: Faker::Artist.name, email: Faker::Internet.email, address: Faker::Address.full_address, pay_method: Faker::Artist.name)
+	line_item = LineItem.create(quantity: 2, item_id: Item.all.sample.id, cart_id: Cart.all.sample.id, order_id: Order.all.sample.id)
 end
